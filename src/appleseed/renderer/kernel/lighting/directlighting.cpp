luisbarrancos/appleseed

commit 59b622f1a21232bd2d9cba370ea926305873fc74

lots of fixes and cleanups regarding handling of back faces, etc.

src/appleseed/renderer/kernel/lighting/directlighting.cpp
@@ -233,8 +233,8 @@ void DirectLightingIntegrator::take_single_bsdf_sample(
     if (bsdf_mode != BSDF::Diffuse)
         return;
 
-    if (bsdf_prob > 0.0)
-        bsdf_value /= static_cast<float>(bsdf_prob);
+    // Since we're limiting ourselves to the diffuse case, the BSDF should not be a Dirac delta.
+    assert(bsdf_prob > 0.0);
 
     // Trace a ray in the direction of the reflection.
     double weight;
@@ -259,6 +259,11 @@ void DirectLightingIntegrator::take_single_bsdf_sample(
     // Retrieve the EDF at the intersection point.
     const EDF* edf = material->get_edf();
     if (edf == 0)
+        return;
+
+    // Cull samples on lights emitting in the wrong direction.
+    const double cos_on = dot(-incoming, light_shading_point.get_shading_normal());
+    if (cos_on <= 0.0)
         return;
 
     // Evaluate the input values of the EDF.
@@ -286,7 +291,6 @@ void DirectLightingIntegrator::take_single_bsdf_sample(
     if (square_distance > 0.0)
     {
         // Transform bsdf_prob to surface area measure (Veach: 8.2.2.2 eq. 8.10).
-        const double cos_on = dot(-incoming, light_shading_point.get_shading_normal());
         const double bsdf_point_prob = bsdf_prob * cos_on / square_distance;
 
         // Compute the probability density wrt. surface area mesure of the light sample.
@@ -302,7 +306,7 @@ void DirectLightingIntegrator::take_single_bsdf_sample(
     }
 
     // Add the contribution of this sample to the illumination.
-    edf_value *= static_cast<float>(weight);
+    edf_value *= static_cast<float>(weight / bsdf_prob);
     edf_value *= bsdf_value;
     radiance += edf_value;
     aovs.add(edf->get_render_layer_index(), radiance);
@@ -357,14 +361,14 @@ void DirectLightingIntegrator::add_emitting_triangle_sample_contribution(
     // Compute the incoming direction in world space.
     Vector3d incoming = sample.m_point - m_point;
 
-    // Cull light samples behind the shading surface.
-    double cos_in = dot(incoming, m_shading_basis.get_normal());
-    if (cos_in <= 0.0)
-        return;
-
     // Cull samples on lights emitting in the wrong direction.
     double cos_on = dot(-incoming, sample.m_shading_normal);
     if (cos_on <= 0.0)
+        return;
+
+    // Compute the transmission factor between the light sample and the shading point.
+    double transmission;
+    if (!check_visibility(sampling_context, sample, transmission))
         return;
 
     // Compute the square distance between the light sample and the shading point.
@@ -373,13 +377,7 @@ void DirectLightingIntegrator::add_emitting_triangle_sample_contribution(
 
     // Normalize the incoming direction.
     incoming *= rcp_sample_distance;
-    cos_in *= rcp_sample_distance;
     cos_on *= rcp_sample_distance;
-
-    // Compute the transmission factor between the light sample and the shading point.
-    double transmission;
-    if (!check_visibility(sampling_context, sample, transmission))
-        return;
 
     // Evaluate the BSDF.
     Spectrum bsdf_value;
@@ -436,13 +434,13 @@ void DirectLightingIntegrator::add_light_sample_contribution(
     Spectrum&               radiance,
     AOVCollection&          aovs)
 {
+    // Compute the transmission factor between the light sample and the shading point.
+    double transmission;
+    if (!check_visibility(sampling_context, sample, transmission))
+        return;
+
     // Compute the incoming direction in world space.
     Vector3d incoming = sample.m_point - m_point;
-
-    // Cull light samples behind the shading surface.
-    double cos_in = dot(incoming, m_shading_basis.get_normal());
-    if (cos_in <= 0.0)
-        return;
 
     // Compute the square distance between the light sample and the shading point.
     const double rcp_sample_square_distance = 1.0 / square_norm(incoming);
@@ -450,16 +448,9 @@ void DirectLightingIntegrator::add_light_sample_contribution(
 
     // Normalize the incoming direction.
     incoming *= rcp_sample_distance;
-    cos_in *= rcp_sample_distance;
-
-    // Compute the transmission factor between the light sample and the shading point.
-    double transmission;
-    if (!check_visibility(sampling_context, sample, transmission))
-        return;
 
     // Evaluate the BSDF.
     Spectrum bsdf_value;
-    double bsdf_prob;
     if (!m_bsdf.evaluate(
             m_bsdf_data,
             false,              // not adjoint
@@ -468,8 +459,7 @@ void DirectLightingIntegrator::add_light_sample_contribution(
             m_shading_basis,
             m_outgoing,
             incoming,
-            bsdf_value,
-            &bsdf_prob))
+            bsdf_value))
         return;
 
     const Light* light = sample.m_light;

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -94,8 +94,8 @@ namespace
             if (bsdf_mode != BSDF::Diffuse)
                 continue;
 
-            if (bsdf_prob > 0.0)
-                bsdf_value /= static_cast<float>(bsdf_prob);
+            // Since we're limiting ourselves to the diffuse case, the BSDF should not be a Dirac delta.
+            assert(bsdf_prob > 0.0);
 
             // Compute the transmission factor toward the incoming direction.
             Tracer tracer(
@@ -127,14 +127,14 @@ namespace
 
             // Compute MIS weight.
             const double mis_weight =
-                bsdf_prob < 0.0
+                bsdf_prob == BSDF::DiracDelta
                     ? 1.0
                     : mis_power2(
                           bsdf_sample_count * bsdf_prob,
                           env_sample_count * env_prob);
 
             // Add the contribution of this sample to the illumination.
-            env_value *= static_cast<float>(transmission * mis_weight);
+            env_value *= static_cast<float>(transmission * mis_weight / bsdf_prob);
             env_value *= bsdf_value;
             radiance += env_value;
         }
@@ -188,12 +188,6 @@ namespace
                 incoming,
                 env_value,
                 env_prob);
-
-            // Cull samples behind the shading surface.
-            assert(is_normalized(incoming));
-            const double cos_in = dot(incoming, shading_basis.get_normal());
-            if (cos_in < 0.0)
-                continue;
 
             // Compute the transmission factor toward the incoming direction.
             SamplingContext child_sampling_context(sampling_context);

src/appleseed/renderer/modeling/bsdf/ashikhminbrdf.cpp
@@ -266,20 +266,12 @@ namespace
                 incoming = force_above_surface(incoming, geometric_normal);
             }
 
-            const Vector3d& shading_normal = shading_basis.get_normal();
-
-            // No reflection in or below the shading surface.
-            const double cos_in = dot(incoming, shading_normal);
-            if (cos_in <= 0.0)
-            {
-                mode = None;
-                return;
-            }
-
             // Compute dot products.
-            const double cos_on = max(dot(outgoing, shading_normal), 1.0e-3);
+            const Vector3d& shading_normal = shading_basis.get_normal();
+            const double cos_in = abs(dot(incoming, shading_normal));
+            const double cos_on = abs(dot(outgoing, shading_normal));
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = max(dot(h, shading_normal), 0.0);
+            const double cos_hn = abs(dot(h, shading_normal));
 
             // Evaluate the glossy component of the BRDF (equation 4).
             const double num = pow(cos_hn, exp);
@@ -319,11 +311,15 @@ namespace
         {
             const Vector3d& shading_normal = shading_basis.get_normal();
 
-            // No reflection in or below the shading surface.
-            const double cos_in = dot(incoming, shading_normal);
-            const double cos_on = dot(outgoing, shading_normal);
-            if (cos_in <= 0.0 || cos_on <= 0.0)
-                return false;
+            double cos_in = dot(incoming, shading_normal);
+            double cos_on = dot(outgoing, shading_normal);
+
+            // The incoming and outgoing directions must be in the same hemisphere.
+            if (cos_in * cos_on <= 0.0)
+                return 0.0;
+
+            cos_in = abs(cos_in);
+            cos_on = abs(cos_on);
 
             const InputValues* values = static_cast<const InputValues*>(data);
 
@@ -341,7 +337,7 @@ namespace
 
             // Compute dot products.
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = max(dot(h, shading_basis.get_normal()), 0.0);
+            const double cos_hn = abs(dot(h, shading_basis.get_normal()));
             const double cos_hu = dot(h, shading_basis.get_tangent_u());
             const double cos_hv = dot(h, shading_basis.get_tangent_v());
 
@@ -395,11 +391,15 @@ namespace
         {
             const Vector3d& shading_normal = shading_basis.get_normal();
 
-            // No reflection in or below the shading surface.
-            const double cos_in = dot(incoming, shading_normal);
-            const double cos_on = dot(outgoing, shading_normal);
-            if (cos_in <= 0.0 || cos_on <= 0.0)
+            double cos_in = dot(incoming, shading_normal);
+            double cos_on = dot(outgoing, shading_normal);
+
+            // The incoming and outgoing directions must be in the same hemisphere.
+            if (cos_in * cos_on <= 0.0)
                 return 0.0;
+
+            cos_in = abs(cos_in);
+            cos_on = abs(cos_on);
 
             const InputValues* values = static_cast<const InputValues*>(data);
 
@@ -417,7 +417,7 @@ namespace
 
             // Compute dot products.
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = max(dot(h, shading_basis.get_normal()), 0.0);
+            const double cos_hn = abs(dot(h, shading_basis.get_normal()));
             const double cos_hu = dot(h, shading_basis.get_tangent_u());
             const double cos_hv = dot(h, shading_basis.get_tangent_v());
 

src/appleseed/renderer/modeling/bsdf/brdfwrapper.h
@@ -116,23 +116,6 @@ void BRDFWrapper<BRDFImpl>::sample(
     assert(foundation::is_normalized(geometric_normal));
     assert(foundation::is_normalized(outgoing));
 
-    // No reflection for back faces.
-    const foundation::Vector3d& shading_normal = shading_basis.get_normal();
-    const double cos_ng = foundation::dot(shading_normal, geometric_normal);
-    if (cos_ng <= 0.0)
-    {
-        mode = BRDFImpl::None;
-        return;
-    }
-
-    // No reflection in or below the geometric surface.
-    const double cos_og = foundation::dot(outgoing, geometric_normal);
-    if (cos_og <= 0.0)
-    {
-        mode = BRDFImpl::None;
-        return;
-    }
-
     BRDFImpl::sample(
         sampling_context,
         data,
@@ -146,17 +129,27 @@ void BRDFWrapper<BRDFImpl>::sample(
         probability,
         mode);
 
+    if (mode == None)
+        return;
+
+    assert(probability == DiracDelta || probability > 0.0);
+
     if (cosine_mult)
     {
         if (adjoint)
         {
-            const double cos_on = std::abs(foundation::dot(outgoing, shading_normal));
             const double cos_ig = foundation::dot(incoming, geometric_normal);
+            const double cos_og = foundation::dot(outgoing, geometric_normal);
+
+            assert(cos_ig >= 0.0);
+            assert(cos_og >= 0.0);
+
+            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             value *= static_cast<float>(cos_on * cos_ig / cos_og);
         }
         else
         {
-            const double cos_in = std::abs(foundation::dot(incoming, shading_normal));
+            const double cos_in = std::abs(foundation::dot(incoming, shading_basis.get_normal()));
             value *= static_cast<float>(cos_in);
         }
     }
@@ -178,15 +171,10 @@ bool BRDFWrapper<BRDFImpl>::evaluate(
     assert(foundation::is_normalized(outgoing));
     assert(foundation::is_normalized(incoming));
 
-    // No reflection for back faces.
-    const foundation::Vector3d& shading_normal = shading_basis.get_normal();
-    const double cos_ng = foundation::dot(shading_normal, geometric_normal);
-    if (cos_ng <= 0.0)
-        return false;
-
-    // No reflection in or below the geometric surface.
     const double cos_ig = foundation::dot(incoming, geometric_normal);
     const double cos_og = foundation::dot(outgoing, geometric_normal);
+
+    // No reflection in or below the geometric surface.
     if (cos_ig <= 0.0 || cos_og <= 0.0)
         return false;
 
@@ -211,12 +199,12 @@ bool BRDFWrapper<BRDFImpl>::evaluate(
     {
         if (adjoint)
         {
-            const double cos_on = std::abs(foundation::dot(outgoing, shading_normal));
+            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             value *= static_cast<float>(cos_on * cos_ig / cos_og);
         }
         else
         {
-            const double cos_in = std::abs(foundation::dot(incoming, shading_normal));
+            const double cos_in = std::abs(foundation::dot(incoming, shading_basis.get_normal()));
             value *= static_cast<float>(cos_in);
         }
     }
@@ -236,15 +224,10 @@ double BRDFWrapper<BRDFImpl>::evaluate_pdf(
     assert(foundation::is_normalized(outgoing));
     assert(foundation::is_normalized(incoming));
 
-    // No reflection for back faces.
-    const foundation::Vector3d& shading_normal = shading_basis.get_normal();
-    const double cos_ng = foundation::dot(shading_normal, geometric_normal);
-    if (cos_ng <= 0.0)
-        return 0.0;
-
-    // No reflection in or below the geometric surface.
     const double cos_ig = foundation::dot(incoming, geometric_normal);
     const double cos_og = foundation::dot(outgoing, geometric_normal);
+
+    // No reflection in or below the geometric surface.
     if (cos_ig <= 0.0 || cos_og <= 0.0)
         return 0.0;
 

commit 3b17363d551fbe480b90eaa079042965cfb0aa5f

reverting incorrect changes introduced in commit 59b622f1a21232bd2d9cba370ea926305873fc74 ("lots of fixes and cleanups regarding handling of back faces, etc.").

src/appleseed/renderer/kernel/lighting/directlighting.cpp
@@ -361,6 +361,11 @@ void DirectLightingIntegrator::add_emitting_triangle_sample_contribution(
     // Compute the incoming direction in world space.
     Vector3d incoming = sample.m_point - m_point;
 
+    // Cull light samples behind the shading surface.
+    double cos_in = dot(incoming, m_shading_basis.get_normal());
+    if (cos_in <= 0.0)
+        return;
+
     // Cull samples on lights emitting in the wrong direction.
     double cos_on = dot(-incoming, sample.m_shading_normal);
     if (cos_on <= 0.0)
@@ -377,6 +382,7 @@ void DirectLightingIntegrator::add_emitting_triangle_sample_contribution(
 
     // Normalize the incoming direction.
     incoming *= rcp_sample_distance;
+    cos_in *= rcp_sample_distance;
     cos_on *= rcp_sample_distance;
 
     // Evaluate the BSDF.
@@ -434,13 +440,18 @@ void DirectLightingIntegrator::add_light_sample_contribution(
     Spectrum&               radiance,
     AOVCollection&          aovs)
 {
+    // Compute the incoming direction in world space.
+    Vector3d incoming = sample.m_point - m_point;
+
+    // Cull light samples behind the shading surface.
+    double cos_in = dot(incoming, m_shading_basis.get_normal());
+    if (cos_in <= 0.0)
+        return;
+
     // Compute the transmission factor between the light sample and the shading point.
     double transmission;
     if (!check_visibility(sampling_context, sample, transmission))
         return;
-
-    // Compute the incoming direction in world space.
-    Vector3d incoming = sample.m_point - m_point;
 
     // Compute the square distance between the light sample and the shading point.
     const double rcp_sample_square_distance = 1.0 / square_norm(incoming);
@@ -448,6 +459,7 @@ void DirectLightingIntegrator::add_light_sample_contribution(
 
     // Normalize the incoming direction.
     incoming *= rcp_sample_distance;
+    cos_in *= rcp_sample_distance;
 
     // Evaluate the BSDF.
     Spectrum bsdf_value;

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -189,6 +189,12 @@ namespace
                 env_value,
                 env_prob);
 
+            // Cull samples behind the shading surface.
+            assert(is_normalized(incoming));
+            const double cos_in = dot(incoming, shading_basis.get_normal());
+            if (cos_in < 0.0)
+                continue;
+
             // Compute the transmission factor toward the incoming direction.
             SamplingContext child_sampling_context(sampling_context);
             Tracer tracer(

src/appleseed/renderer/modeling/bsdf/ashikhminbrdf.cpp
@@ -266,12 +266,20 @@ namespace
                 incoming = force_above_surface(incoming, geometric_normal);
             }
 
-            // Compute dot products.
             const Vector3d& shading_normal = shading_basis.get_normal();
-            const double cos_in = abs(dot(incoming, shading_normal));
-            const double cos_on = abs(dot(outgoing, shading_normal));
+
+            // No reflection in or below the shading surface.
+            const double cos_in = dot(incoming, shading_normal);
+            if (cos_in <= 0.0)
+            {
+                mode = None;
+                return;
+            }
+
+            // Compute dot products.
+            const double cos_on = max(dot(outgoing, shading_normal), 0.0);
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = abs(dot(h, shading_normal));
+            const double cos_hn = max(dot(h, shading_normal), 0.0);
 
             // Evaluate the glossy component of the BRDF (equation 4).
             const double num = pow(cos_hn, exp);
@@ -311,15 +319,11 @@ namespace
         {
             const Vector3d& shading_normal = shading_basis.get_normal();
 
-            double cos_in = dot(incoming, shading_normal);
-            double cos_on = dot(outgoing, shading_normal);
-
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (cos_in * cos_on <= 0.0)
-                return 0.0;
-
-            cos_in = abs(cos_in);
-            cos_on = abs(cos_on);
+            // No reflection in or below the shading surface.
+            const double cos_in = dot(incoming, shading_normal);
+            const double cos_on = dot(outgoing, shading_normal);
+            if (cos_in <= 0.0 || cos_on <= 0.0)
+                return false;
 
             const InputValues* values = static_cast<const InputValues*>(data);
 
@@ -337,7 +341,7 @@ namespace
 
             // Compute dot products.
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = abs(dot(h, shading_basis.get_normal()));
+            const double cos_hn = max(dot(h, shading_basis.get_normal()), 0.0);
             const double cos_hu = dot(h, shading_basis.get_tangent_u());
             const double cos_hv = dot(h, shading_basis.get_tangent_v());
 
@@ -391,15 +395,11 @@ namespace
         {
             const Vector3d& shading_normal = shading_basis.get_normal();
 
-            double cos_in = dot(incoming, shading_normal);
-            double cos_on = dot(outgoing, shading_normal);
-
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (cos_in * cos_on <= 0.0)
+            // No reflection in or below the shading surface.
+            const double cos_in = dot(incoming, shading_normal);
+            const double cos_on = dot(outgoing, shading_normal);
+            if (cos_in <= 0.0 || cos_on <= 0.0)
                 return 0.0;
-
-            cos_in = abs(cos_in);
-            cos_on = abs(cos_on);
 
             const InputValues* values = static_cast<const InputValues*>(data);
 
@@ -417,7 +417,7 @@ namespace
 
             // Compute dot products.
             const double cos_oh = max(dot(outgoing, h), 1.0e-3);
-            const double cos_hn = abs(dot(h, shading_basis.get_normal()));
+            const double cos_hn = max(dot(h, shading_basis.get_normal()), 0.0);
             const double cos_hu = dot(h, shading_basis.get_tangent_u());
             const double cos_hv = dot(h, shading_basis.get_tangent_v());
 

src/appleseed/renderer/modeling/bsdf/brdfwrapper.h
@@ -115,6 +115,7 @@ void BRDFWrapper<BRDFImpl>::sample(
 {
     assert(foundation::is_normalized(geometric_normal));
     assert(foundation::is_normalized(outgoing));
+    assert(foundation::dot(outgoing, geometric_normal) >= 0.0);
 
     BRDFImpl::sample(
         sampling_context,
@@ -132,19 +133,17 @@ void BRDFWrapper<BRDFImpl>::sample(
     if (mode == None)
         return;
 
+    assert(foundation::is_normalized(incoming));
+    assert(foundation::dot(incoming, geometric_normal) >= 0.0);
     assert(probability == DiracDelta || probability > 0.0);
 
     if (cosine_mult)
     {
         if (adjoint)
         {
+            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             const double cos_ig = foundation::dot(incoming, geometric_normal);
             const double cos_og = foundation::dot(outgoing, geometric_normal);
-
-            assert(cos_ig >= 0.0);
-            assert(cos_og >= 0.0);
-
-            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             value *= static_cast<float>(cos_on * cos_ig / cos_og);
         }
         else

src/appleseed/renderer/modeling/bsdf/btdfwrapper.h
@@ -132,13 +132,16 @@ void BTDFWrapper<BTDFImpl>::sample(
     if (mode == None)
         return;
 
+    assert(foundation::is_normalized(incoming));
+    assert(probability == DiracDelta || probability > 0.0);
+
     if (cosine_mult)
     {
         if (adjoint)
         {
+            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             const double cos_ig = std::abs(foundation::dot(incoming, geometric_normal));
             const double cos_og = std::abs(foundation::dot(outgoing, geometric_normal));
-            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             value *= static_cast<float>(cos_on * cos_ig / cos_og);
         }
         else
@@ -186,9 +189,9 @@ bool BTDFWrapper<BTDFImpl>::evaluate(
     {
         if (adjoint)
         {
+            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             const double cos_ig = std::abs(foundation::dot(incoming, geometric_normal));
             const double cos_og = std::abs(foundation::dot(outgoing, geometric_normal));
-            const double cos_on = std::abs(foundation::dot(outgoing, shading_basis.get_normal()));
             value *= static_cast<float>(cos_on * cos_ig / cos_og);
         }
         else

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -173,7 +173,7 @@ namespace
             const Vector3d& N = shading_basis.get_normal();
 
             // Compute the outgoing angle.
-            const double dot_VN = abs(dot(V, N));
+            const double dot_VN = max(dot(V, N), 0.0);
 
             // Compute the specular albedo for the outgoing angle.
             Spectrum specular_albedo_V;
@@ -212,8 +212,8 @@ namespace
                 H = normalize(incoming + V);
 
                 dot_LN = wi.y;
-                dot_HN = abs(dot(H, N));
-                dot_HV = abs(dot(H, V));
+                dot_HN = max(dot(H, N), 0.0);
+                dot_HV = dot(H, V);
             }
             else if (s[2] < matte_prob + specular_prob)
             {
@@ -225,13 +225,20 @@ namespace
                 // Transform the halfway vector to parent space.
                 H = shading_basis.transform_to_parent(local_H);
 
-                dot_HV = abs(dot(H, V));
+                dot_HV = dot(H, V);
 
                 // The incoming direction is the reflection of V around H.
                 incoming = (dot_HV + dot_HV) * H - V;
 
-                dot_LN = abs(dot(incoming, N));
+                dot_LN = dot(incoming, N);
                 dot_HN = local_H.y;
+
+                // No reflection in or below the shading surface.
+                if (dot_LN <= 0.0)
+                {
+                    mode = None;
+                    return;
+                }
             }
             else
             {
@@ -296,20 +303,17 @@ namespace
             const Vector3d& L = incoming;
             const Vector3d& N = shading_basis.get_normal();
 
-            double dot_VN = dot(V, N);
-            double dot_LN = dot(L, N);
+            const double dot_VN = dot(V, N);
+            const double dot_LN = dot(L, N);
 
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (dot_VN * dot_LN <= 0.0)
-                return 0.0;
-
-            dot_VN = abs(dot_VN);
-            dot_LN = abs(dot_LN);
+            // No reflection in or below the shading surface.
+            if (dot_LN <= 0.0 || dot_VN <= 0.0)
+                return false;
 
             // Compute the halfway vector.
             const Vector3d H = normalize(L + V);
-            const double dot_HN = abs(dot(H, N));
-            const double dot_HL = abs(dot(H, L));
+            const double dot_HN = dot(H, N);
+            const double dot_HL = dot(H, L);
 
             // Compute the specular albedos for the outgoing and incoming angles.
             Spectrum specular_albedo_V, specular_albedo_L;
@@ -373,15 +377,12 @@ namespace
             const Vector3d& L = incoming;
             const Vector3d& N = shading_basis.get_normal();
 
-            double dot_VN = dot(V, N);
-            double dot_LN = dot(L, N);
+            const double dot_VN = dot(V, N);
+            const double dot_LN = dot(L, N);
 
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (dot_VN * dot_LN <= 0.0)
+            // No reflection in or below the shading surface.
+            if (dot_LN <= 0.0 || dot_VN <= 0.0)
                 return 0.0;
-
-            dot_VN = abs(dot_VN);
-            dot_LN = abs(dot_LN);
 
             // Compute the halfway vector.
             const Vector3d H = normalize(L + V);

src/appleseed/renderer/modeling/bsdf/lambertianbrdf.cpp
@@ -166,8 +166,8 @@ namespace
             const double cos_in = dot(incoming, n);
             const double cos_on = dot(outgoing, n);
 
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (cos_in * cos_on <= 0.0)
+            // No reflection in or below the shading surface.
+            if (cos_in <= 0.0 || cos_on <= 0.0)
                 return false;
 
             // Compute the BRDF value.
@@ -182,7 +182,7 @@ namespace
 
             // Compute the probability density of the sampled direction.
             if (probability)
-                *probability = abs(cos_in) * RcpPi;
+                *probability = cos_in * RcpPi;
 
             return true;
         }
@@ -198,11 +198,11 @@ namespace
             const double cos_in = dot(incoming, n);
             const double cos_on = dot(outgoing, n);
 
-            // The incoming and outgoing directions must be in the same hemisphere.
-            if (cos_in * cos_on <= 0.0)
+            // No reflection in or below the shading surface.
+            if (cos_in <= 0.0 || cos_on <= 0.0)
                 return 0.0;
 
-            return abs(cos_in) * RcpPi;
+            return cos_in * RcpPi;
         }
 
       private:

src/appleseed/renderer/modeling/edf/diffuseedf.cpp
@@ -97,6 +97,7 @@ namespace
             value = values->m_exitance;
 
             probability = wo.y * RcpPi;
+            assert(probability > 0.0);
         }
 
         virtual void evaluate(
@@ -108,6 +109,15 @@ namespace
         {
             assert(is_normalized(geometric_normal));
             assert(is_normalized(outgoing));
+
+            const double cos_on = dot(outgoing, shading_basis.get_normal());
+
+            // No emission in or below the shading surface.
+            if (cos_on <= 0.0)
+            {
+                value.set(0.0f);
+                return;
+            }
 
             const InputValues* values = static_cast<const InputValues*>(data);
             value = values->m_exitance;
@@ -124,10 +134,19 @@ namespace
             assert(is_normalized(geometric_normal));
             assert(is_normalized(outgoing));
 
+            const double cos_on = dot(outgoing, shading_basis.get_normal());
+
+            // No emission in or below the shading surface.
+            if (cos_on <= 0.0)
+            {
+                value.set(0.0f);
+                probability = 0.0;
+                return;
+            }
+
             const InputValues* values = static_cast<const InputValues*>(data);
             value = values->m_exitance;
 
-            const double cos_on = abs(dot(outgoing, shading_basis.get_normal()));
             probability = cos_on * RcpPi;
         }
 
@@ -140,7 +159,12 @@ namespace
             assert(is_normalized(geometric_normal));
             assert(is_normalized(outgoing));
 
-            const double cos_on = abs(dot(outgoing, shading_basis.get_normal()));
+            const double cos_on = dot(outgoing, shading_basis.get_normal());
+
+            // No emission in or below the shading surface.
+            if (cos_on <= 0.0)
+                return 0.0;
+
             return cos_on * RcpPi;
         }
 