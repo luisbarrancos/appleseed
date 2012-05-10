luisbarrancos/appleseed

commit 59b622f1a21232bd2d9cba370ea926305873fc74

lots of fixes and cleanups regarding handling of back faces, etc.

src/appleseed/renderer/kernel/lighting/drt/drt.cpp
@@ -224,6 +224,7 @@ namespace
                 const Vector3d& shading_normal = shading_point.get_shading_normal();
                 const Basis3d& shading_basis = shading_point.get_shading_basis();
                 const Material* material = shading_point.get_material();
+                const EDF* edf = material->get_edf();
 
                 // Compute direct lighting. We're sampling both the lights and the BSDF,
                 // unlike in the path tracer where we're only sampling the lights.
@@ -276,10 +277,7 @@ namespace
                     vertex_aovs.add(m_env_edf->get_render_layer_index(), ibl_radiance);
                 }
 
-                const EDF* edf = material->get_edf();
-                const double cos_on = dot(outgoing, shading_normal);
-
-                if (edf && cos_on > 0.0)
+                if (edf)
                 {
                     // Evaluate the input values of the EDF.
                     InputEvaluator edf_input_evaluator(m_texture_cache);
@@ -302,6 +300,7 @@ namespace
                     if (prev_bsdf_mode != BSDF::Specular && square_distance > 0.0)
                     {
                         // Transform prev_bsdf_prob to surface area measure (Veach: 8.2.2.2 eq. 8.10).
+                        const double cos_on = abs(dot(outgoing, shading_normal));
                         const double bsdf_point_prob = prev_bsdf_prob * cos_on / square_distance;
 
                         // Compute the probability density wrt. surface area of choosing this point

src/appleseed/renderer/kernel/lighting/pathtracing/pathtracing.cpp
@@ -231,9 +231,7 @@ namespace
                 const Vector3d& shading_normal = shading_point.get_shading_normal();
                 const Basis3d& shading_basis = shading_point.get_shading_basis();
                 const Material* material = shading_point.get_material();
-
                 const EDF* edf = material->get_edf();
-                const double cos_on = dot(outgoing, shading_normal);
 
                 // Evaluate the input values of the EDF (if any).
                 InputEvaluator edf_input_evaluator(m_texture_cache);
@@ -293,7 +291,7 @@ namespace
                         vertex_aovs.add(m_env_edf->get_render_layer_index(), ibl_radiance);
                     }
 
-                    if (edf && cos_on > 0.0)
+                    if (edf)
                     {
                         // Compute the emitted radiance.
                         Spectrum emitted_radiance;
@@ -309,6 +307,7 @@ namespace
                         if (prev_bsdf_mode != BSDF::Specular && square_distance > 0.0)
                         {
                             // Transform prev_bsdf_prob to surface area measure (Veach: 8.2.2.2 eq. 8.10).
+                            const double cos_on = abs(dot(outgoing, shading_normal));
                             const double bsdf_point_prob = prev_bsdf_prob * cos_on / square_distance;
 
                             // Compute the probability density wrt. surface area of choosing this point
@@ -333,24 +332,21 @@ namespace
                     vertex_aovs *= throughput;
                     m_path_aovs += vertex_aovs;
                 }
-                else
+                else if (edf)
                 {
-                    if (edf && cos_on > 0.0)
-                    {
-                        // Compute the emitted radiance.
-                        Spectrum emitted_radiance;
-                        edf->evaluate(
-                            edf_data,
-                            geometric_normal,
-                            shading_basis,
-                            outgoing,
-                            emitted_radiance);
+                    // Compute the emitted radiance.
+                    Spectrum emitted_radiance;
+                    edf->evaluate(
+                        edf_data,
+                        geometric_normal,
+                        shading_basis,
+                        outgoing,
+                        emitted_radiance);
 
-                        // Update the path radiance.
-                        emitted_radiance *= throughput;
-                        m_path_radiance += emitted_radiance;
-                        m_path_aovs.add(edf->get_render_layer_index(), emitted_radiance);
-                    }
+                    // Update the path radiance.
+                    emitted_radiance *= throughput;
+                    m_path_radiance += emitted_radiance;
+                    m_path_aovs.add(edf->get_render_layer_index(), emitted_radiance);
                 }
 
                 // Proceed with this path.

sandbox/tests/test scenes/back faces/02 back faces specular reflection.appleseed
@@ -1,0 +1,215 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- File generated by appleseed.dll version 1.1.0-alpha-12-24-g7ad29e2. -->
+<project>
+    <scene>
+        <camera name="camera" model="pinhole_camera">
+            <parameter name="controller_target" value="-0.00597066 0.155329 0.0616099" />
+            <parameter name="film_dimensions" value="0.024892 0.018669" />
+            <parameter name="focal_length" value="0.035" />
+            <transform time="0">
+                <matrix>
+                    0.999717723335142 -0.012705571103431 0.020075908759914 0.033869991388236
+                    0.000000000000000 0.844993516352401 0.534776548964523 1.216592911242183
+                    -0.023758654204326 -0.534625594023837 0.844754994400778 1.738026500139385
+                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                </matrix>
+            </transform>
+        </camera>
+        <environment name="environment" model="generic_environment" />
+        <assembly name="assembly">
+            <color name="light_exitance">
+                <parameter name="alpha" value="1.0" />
+                <parameter name="color" value="1 1 1" />
+                <parameter name="color_space" value="srgb" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    1.000000 1.000000 1.000000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <color name="light_reflectance">
+                <parameter name="alpha" value="1.0" />
+                <parameter name="color" value="0.5" />
+                <parameter name="color_space" value="spectral" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    0.500000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <color name="left_square_reflectance">
+                <parameter name="alpha" value="1.0" />
+                <parameter name="color" value="1 0 0" />
+                <parameter name="color_space" value="srgb" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    1.000000 0.000000 0.000000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <color name="right_square_reflectance">
+                <parameter name="alpha" value="1.0" />
+                <parameter name="color" value="0 1 0" />
+                <parameter name="color_space" value="srgb" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    0.000000 1.000000 0.000000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <color name="vertical_square_reflectance">
+                <parameter name="alpha" value="1.0" />
+                <parameter name="color" value="0 0 1" />
+                <parameter name="color_space" value="srgb" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    0.000000 0.000000 1.000000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <color name="back_reflectance">
+                <parameter name="alpha" value="1" />
+                <parameter name="color" value="1 1 1" />
+                <parameter name="color_space" value="srgb" />
+                <parameter name="multiplier" value="1.0" />
+                <parameter name="wavelength_range" value="400.0 700.0" />
+                <values>
+                    1.000000 1.000000 1.000000
+                </values>
+                <alpha>
+                    1.000000
+                </alpha>
+            </color>
+            <bsdf name="light_brdf" model="lambertian_brdf">
+                <parameter name="reflectance" value="light_reflectance" />
+            </bsdf>
+            <bsdf name="left_square_brdf" model="lambertian_brdf">
+                <parameter name="reflectance" value="left_square_reflectance" />
+            </bsdf>
+            <bsdf name="vertical_square_brdf" model="lambertian_brdf">
+                <parameter name="reflectance" value="vertical_square_reflectance" />
+            </bsdf>
+            <bsdf name="right_square_brdf" model="lambertian_brdf">
+                <parameter name="reflectance" value="right_square_reflectance" />
+            </bsdf>
+            <bsdf name="back_brdf" model="specular_brdf">
+                <parameter name="reflectance" value="back_reflectance" />
+            </bsdf>
+            <edf name="light_edf" model="diffuse_edf">
+                <parameter name="exitance" value="light_exitance" />
+            </edf>
+            <surface_shader name="physical_surface_shader" model="physical_surface_shader">
+                <parameter name="aerial_persp_distance" value="1000.0" />
+                <parameter name="aerial_persp_intensity" value="0.01" />
+                <parameter name="aerial_persp_mode" value="none" />
+            </surface_shader>
+            <material name="light_material" model="generic_material">
+                <parameter name="bsdf" value="light_brdf" />
+                <parameter name="edf" value="light_edf" />
+                <parameter name="surface_shader" value="physical_surface_shader" />
+            </material>
+            <material name="left_square_material" model="generic_material">
+                <parameter name="bsdf" value="left_square_brdf" />
+                <parameter name="surface_shader" value="physical_surface_shader" />
+            </material>
+            <material name="vertical_square_material" model="generic_material">
+                <parameter name="bsdf" value="vertical_square_brdf" />
+                <parameter name="surface_shader" value="physical_surface_shader" />
+            </material>
+            <material name="right_square_material" model="generic_material">
+                <parameter name="bsdf" value="right_square_brdf" />
+                <parameter name="surface_shader" value="physical_surface_shader" />
+            </material>
+            <material name="back_material" model="generic_material">
+                <parameter name="bsdf" value="back_brdf" />
+                <parameter name="surface_shader" value="physical_surface_shader" />
+            </material>
+            <object name="back faces" model="mesh_object">
+                <parameter name="filename" value="back faces.obj" />
+            </object>
+            <object_instance name="back faces.vertical_square_inst" object="back faces.vertical_square">
+                <transform>
+                    <matrix>
+                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="0" side="front" material="vertical_square_material" />
+                <assign_material slot="0" side="back" material="back_material" />
+            </object_instance>
+            <object_instance name="back faces.right_square_inst" object="back faces.right_square">
+                <transform>
+                    <matrix>
+                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="0" side="front" material="right_square_material" />
+                <assign_material slot="0" side="back" material="back_material" />
+            </object_instance>
+            <object_instance name="back faces.left_square_inst" object="back faces.left_square">
+                <transform>
+                    <matrix>
+                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="0" side="front" material="left_square_material" />
+            </object_instance>
+            <object_instance name="back faces.light_inst" object="back faces.light">
+                <transform>
+                    <matrix>
+                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="0" side="front" material="light_material" />
+                <assign_material slot="0" side="back" material="back_material" />
+            </object_instance>
+        </assembly>
+        <assembly_instance name="assembly_inst" assembly="assembly">
+            <transform>
+                <matrix>
+                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                </matrix>
+            </transform>
+        </assembly_instance>
+    </scene>
+    <output>
+        <frame name="beauty">
+            <parameter name="camera" value="camera" />
+            <parameter name="color_space" value="srgb" />
+            <parameter name="resolution" value="640 480" />
+        </frame>
+    </output>
+    <configurations>
+        <configuration name="final" base="base_final" />
+        <configuration name="interactive" base="base_interactive" />
+    </configurations>
+</project>

commit bd872ba97864ec5bae7c5d461fceec0a17c953d7

reverting incorrect changes to DRT and PT introduced in commit 59b622f1a21232bd2d9cba370ea926305873fc74 ("lots of fixes and cleanups regarding handling of back faces, etc.").

src/appleseed/renderer/kernel/lighting/drt/drt.cpp
@@ -224,7 +224,6 @@ namespace
                 const Vector3d& shading_normal = shading_point.get_shading_normal();
                 const Basis3d& shading_basis = shading_point.get_shading_basis();
                 const Material* material = shading_point.get_material();
-                const EDF* edf = material->get_edf();
 
                 // Compute direct lighting. We're sampling both the lights and the BSDF,
                 // unlike in the path tracer where we're only sampling the lights.
@@ -277,7 +276,10 @@ namespace
                     vertex_aovs.add(m_env_edf->get_render_layer_index(), ibl_radiance);
                 }
 
-                if (edf)
+                const EDF* edf = material->get_edf();
+                const double cos_on = dot(outgoing, shading_normal);
+
+                if (edf && cos_on > 0.0)
                 {
                     // Evaluate the input values of the EDF.
                     InputEvaluator edf_input_evaluator(m_texture_cache);
@@ -300,7 +302,6 @@ namespace
                     if (prev_bsdf_mode != BSDF::Specular && square_distance > 0.0)
                     {
                         // Transform prev_bsdf_prob to surface area measure (Veach: 8.2.2.2 eq. 8.10).
-                        const double cos_on = abs(dot(outgoing, shading_normal));
                         const double bsdf_point_prob = prev_bsdf_prob * cos_on / square_distance;
 
                         // Compute the probability density wrt. surface area of choosing this point

src/appleseed/renderer/kernel/lighting/pathtracing/pathtracing.cpp
@@ -231,7 +231,9 @@ namespace
                 const Vector3d& shading_normal = shading_point.get_shading_normal();
                 const Basis3d& shading_basis = shading_point.get_shading_basis();
                 const Material* material = shading_point.get_material();
+
                 const EDF* edf = material->get_edf();
+                const double cos_on = dot(outgoing, shading_normal);
 
                 // Evaluate the input values of the EDF (if any).
                 InputEvaluator edf_input_evaluator(m_texture_cache);
@@ -291,7 +293,7 @@ namespace
                         vertex_aovs.add(m_env_edf->get_render_layer_index(), ibl_radiance);
                     }
 
-                    if (edf)
+                    if (edf && cos_on > 0.0)
                     {
                         // Compute the emitted radiance.
                         Spectrum emitted_radiance;
@@ -307,7 +309,6 @@ namespace
                         if (prev_bsdf_mode != BSDF::Specular && square_distance > 0.0)
                         {
                             // Transform prev_bsdf_prob to surface area measure (Veach: 8.2.2.2 eq. 8.10).
-                            const double cos_on = abs(dot(outgoing, shading_normal));
                             const double bsdf_point_prob = prev_bsdf_prob * cos_on / square_distance;
 
                             // Compute the probability density wrt. surface area of choosing this point
@@ -332,21 +333,24 @@ namespace
                     vertex_aovs *= throughput;
                     m_path_aovs += vertex_aovs;
                 }
-                else if (edf)
+                else
                 {
-                    // Compute the emitted radiance.
-                    Spectrum emitted_radiance;
-                    edf->evaluate(
-                        edf_data,
-                        geometric_normal,
-                        shading_basis,
-                        outgoing,
-                        emitted_radiance);
+                    if (edf && cos_on > 0.0)
+                    {
+                        // Compute the emitted radiance.
+                        Spectrum emitted_radiance;
+                        edf->evaluate(
+                            edf_data,
+                            geometric_normal,
+                            shading_basis,
+                            outgoing,
+                            emitted_radiance);
 
-                    // Update the path radiance.
-                    emitted_radiance *= throughput;
-                    m_path_radiance += emitted_radiance;
-                    m_path_aovs.add(edf->get_render_layer_index(), emitted_radiance);
+                        // Update the path radiance.
+                        emitted_radiance *= throughput;
+                        m_path_radiance += emitted_radiance;
+                        m_path_aovs.add(edf->get_render_layer_index(), emitted_radiance);
+                    }
                 }
 
                 // Proceed with this path.